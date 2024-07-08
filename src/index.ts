851538359268d18a56149ece6c040e62eba03ee8
Phil Chan <phillipchan1@gmail.com>
2024-07-07 22:58:04 -0500

    Update success message

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ app.post(
 
       await addTradeToDatabase(notionDatabaseId, trade);
 
-      res.send("Trade added with image analysis");
+      res.send("Trade added to database!");
     } catch (error) {
       console.error("Error processing request:", error);
       res.status(500).send("An error occurred while processing the request");
